1006730ba0254c814e543dd23ca64a3e614df2cd
lJOKERl <myweb.joker@gmail.com>
2019-09-16 17:58:39 +0300

    добавлены блоки объявлений с картинками

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
-    <v-container>
+    <v-container fluid> 
       <v-layout row>
           <v-flex xs12>
-            <h1>Главная</h1>
             <v-carousel>
               <v-carousel-item
-                v-for="(item,i) in items"
-                :key="i"
-                :src="item.src"
+                v-for="ad in ads"
+                :key="ad.id"
+                :src="ad.image"
                 reverse-transition="fade-transition"
                 transition="fade-transition"
               ></v-carousel-item>
             </v-carousel>
+
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              v-for="ad in ads"
+              :key="ad.id"
+              
+            >
+              <v-img
+                class="white--text"
+                height="200px"
+                :src="ad.image"
+              >
+                <v-card-title class="align-end fill-height">{{ad.title}}</v-card-title>
+              </v-img>
+
+              <v-card-text>
+                <span class="text--primary">
+                  <span>{{ad.description}}</span><br>
+                </span>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="orange"
+                >
+                  Открыть
+                </v-btn>
+                <v-btn
+                  text
+                  raised
+                  color="orange"
+                >
+                  Купить
+                </v-btn>
+              </v-card-actions>
+            </v-card>
           </v-flex>
       </v-layout>
     </v-container>
@@ -23,9 +60,38 @@
   export default {
     data() {
       return {
-        
+        ads:[
+          {
+            title: 'Первое объявление', 
+            description: 'Lorem ipsum',
+            promo: false,
+            image: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            id: '1'
+          },
+          {
+            title: 'Второе объявление', 
+            description: 'Lorem ipsum',
+            promo: false,
+            image: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            id: '2'
+          },
+          {
+            title: 'Третье объявление', 
+            description: 'Lorem ipsum',
+            promo: false,
+            image: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            id: '3'
+          },
+        ]
       }
     }
   }
 </script>
+
+<style >
+  .container,
+  .container--fluid {
+    padding: 0!important;
+  }
+</style>
 
